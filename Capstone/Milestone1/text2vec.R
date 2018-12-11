c03cee535e192e9d4fd0d76e3a6cda8d7796f9f4
lukas <aprilium@live.com>
2018-12-11 17:33:50 +0200

    add Zips

--- Capstone/Milestone1/text2vec.R
+++ Capstone/Milestone1/text2vec.R
@@ -5,31 +5,36 @@ library(doParallel)
 N_WORKERS = 4
 registerDoParallel(N_WORKERS)
 t1 <- Sys.time()
+file_type <-"twitter"
+text.clean = function(x)                    # text data
+{ require("tm")
+        x  =  gsub("<.*?>", " ", x)               # regex for removing HTML tags
+        x  =  iconv(x, "latin1", "ASCII", sub="") # Keep only ASCII characters
+        x  =  gsub("[^[:alnum:]]", " ", x)        # keep only alpha numeric 
+        x  =  tolower(x)                          # convert to lower case characters
+        x  =  removeNumbers(x)                    # removing numbers
+        x  =  stripWhitespace(x)                  # removing white space
+        return(x)
+}
+
 it_files_par = ifiles_parallel(file_paths = 
-       c("/data/final/en_US/en_US.twitter.txt"))
-it_token_par = itoken_parallel(it_files_par, preprocessor = tolower, tokenizer = word_tokenizer)
-vocab = create_vocabulary(it_token_par)
-vocab <-vocab %>%  mutate(term = str_extract(term, "[a-z']+"))
-vocab<-vocab[complete.cases(vocab),]
+       paste0("/data/final/en_US/en_US.",file_type,".txt"))
+it_token_par = itoken_parallel(it_files_par, preprocessor = text.clean, tokenizer = word_tokenizer)
+vocab = create_vocabulary(it_token_par,stopwords = as.vector(bad_words$V1))
 print(difftime(Sys.time(),t1,units = 'sec'))
 
-# DTM vocabulary vectorization
-v_vectorizer = vocab_vectorizer(vocab)
-dtm_v = create_dtm(it_token_par, vectorizer = v_vectorizer)
-# DTM hash vectorization
-h_vectorizer = hash_vectorizer()
-dtm_h = create_dtm(it_token_par, vectorizer = h_vectorizer)
-# co-ocurence statistics
-tcm = create_tcm(it_token_par, vectorizer = v_vectorizer, skip_grams_window = 5)
 
-#check vulgarities, call vulgarities()
-vul<-vulgarities()
-z<-vocab[vocab$term %in% vul,]
-length(z)
+z<-vocab %>% arrange(desc(term_count)) %>% mutate(rank=row_number(),term_frequency=term_count/sum(term_count))
+z %>% ggplot(aes(rank, term_frequency),options(scipen=999)) +
+        geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) #+
+        scale_x_log10(labels = function(x) format(x, scientific = FALSE)) +
+        scale_y_log10(labels = function(x) format(x, scientific = FALSE))
 
 
-#another way to obtain list of profanity
+
+
+
+#obtain list of profanity and extract them from the text
 download.file("http://www.bannedwordlist.com/lists/swearWords.txt",destfile = "bad_words.txt")
 bad_words<-read.delim("bad_words.txt",header = FALSE)
-z<-vocab[vocab$term %in% bad_words$V1,]
-sum(z$term_count)
+
