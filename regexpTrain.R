021bf7b3cc7a4e37cac37eb33597085f870cd35a
lukas <aprilium@live.com>
2018-12-17 18:07:22 +0200

    add comments

--- regexpTrain.R
+++ regexpTrain.R
@@ -2,22 +2,28 @@ Sys.setlocale("LC_ALL",locale = "Ukrainian")
 
 library(pdftools)
 download.file("https://www.gubkin.ru/intranet/docs_smk/ISO_9001-2015(R).pdf","iso.pdf")
+##could not open the pdf
 text<- read_lines("New Text Document.txt")
+#find number of line with pattern
 listISO<-grep("-\\s\\ISO\\s\\d{4}|-\\s\\ISO/TR\\s\\d{5}",text)
 ISOtext<-vector()
+#create list of looking paragpaphes
 for (i in 1:17){
         k<-listISO[i+1]-1
         m<-listISO[i]
         zeta<-str_flatten(text[m:k],collapse = " ")
         ISOtext <-c(ISOtext,zeta)
 }
+#my loop can't work with the last line
 ISOtext[18]<-str_flatten(text[listISO[18]:109],collapse = " ")
+
+#data frame
 df<-data.frame(matrix(nrow=18,ncol=2))
 df[,1]<-str_extract(ISOtext,"-\\s\\ISO\\s\\d{4,5}|-\\s\\ISO/TR\\s\\d{5}")
 df[,2]<-str_remove(ISOtext,"-\\s\\ISO\\s\\d{4,5}|-\\s\\ISO/TR\\s\\d{5}")
 df[,1]<-str_remove(df[,1],"- ")
 
-
+#the same for the other part of text
 listISO<-grep("\\[\\d",text)
 ISOtext<-vector()
 for (i in 1:22){
